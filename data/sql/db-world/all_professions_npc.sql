150dd065d5b6d1e894eb6a59e73b07389a4f3199
rfahey79 <ryan.fahey@gmail.com>
2026-04-11 15:09:33 -0700

    Update all_professions_npc.sql
    Initial setup

--- data/sql/db-world/all_professions_npc.sql
+++ data/sql/db-world/all_professions_npc.sql
@@ -18,22 +18,3 @@ VALUES
     0, 1, 0, 7, 0,
     1, 12340, 'npc_all_professions_trainer'
 );
-
--- Example spawn in Stormwind
-DELETE FROM `creature` WHERE `id1` = @ENTRY;
-INSERT INTO `creature`
-(
-    `guid`, `id1`, `map`, `zoneId`, `areaId`,
-    `spawnMask`, `phaseMask`, `equipment_id`,
-    `position_x`, `position_y`, `position_z`, `orientation`,
-    `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`,
-    `MovementType`
-)
-VALUES
-(
-    900001, @ENTRY, 0, 1519, 1519,
-    1, 1, 1,
-    -8850.0, 640.0, 95.0, 0.0,
-    300, 0, 0, 5000, 0,
-    0
-);
