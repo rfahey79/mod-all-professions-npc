d187eb1fc6c2a5a605b577f3c06d6c0e5b8640be
rfahey79 <ryan.fahey@gmail.com>
2026-04-11 19:21:20 -0700

    Update all_professions_npc.sql
    The template was outdated; copied the transmogrifier template

--- data/sql/db-world/all_professions_npc.sql
+++ data/sql/db-world/all_professions_npc.sql
@@ -1,20 +1,7 @@
 -- Pick an unused creature_template entry ID
 SET @ENTRY := 900001;
+SET @NAME := 'Profession Master';
 
 DELETE FROM `creature_template` WHERE `entry` = @ENTRY;
-INSERT INTO `creature_template`
-(
-    `entry`, `modelid1`, `name`, `subname`, `IconName`,
-    `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`,
-    `npcflag`, `speed_walk`, `speed_run`, `scale`,
-    `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`,
-    `RegenHealth`, `VerifiedBuild`, `ScriptName`
-)
-VALUES
-(
-    @ENTRY, 19723, 'Profession Master', 'Learn any profession', '',
-    0, 80, 80, 35,
-    1, 1, 1.14286, 1,
-    0, 1, 0, 7, 0,
-    1, 12340, 'npc_all_professions_trainer'
-);
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`,`unit_class`, `unit_flags`,`type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `CreatureImmunitiesId`, `flags_extra`, `ScriptName`>
+(@ENTRY, @NAME, "Learn any profession", NULL, 0, 80, 80, 2, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'npc_all_professions_trainer');
