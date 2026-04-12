8a62552fbf3a1cf2067e5250b6850172ccaba1bb
rfahey79 <ryan.fahey@gmail.com>
2026-04-11 19:26:45 -0700

    Update all_professions_npc.sql
    Missed "VALUES" keyword

--- data/sql/db-world/all_professions_npc.sql
+++ data/sql/db-world/all_professions_npc.sql
@@ -1,7 +1,39 @@
 -- Pick an unused creature_template entry ID
-SET @ENTRY := 900001;
+SET @ENTRY := 190011;
 SET @NAME := 'Profession Master';
 
 DELETE FROM `creature_template` WHERE `entry` = @ENTRY;
-INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`,`unit_class`, `unit_flags`,`type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `CreatureImmunitiesId`, `flags_extra`, `ScriptName`)
+INSERT INTO `creature_template` 
+    (
+        `entry`,
+        `name`,
+        `subname`,
+        `IconName`,
+        `gossip_menu_id`,
+        `minlevel`,
+        `maxlevel`,
+        `exp`,
+        `faction`,
+        `npcflag`,
+        `rank`,
+        `dmgschool`,
+        `BaseAttackTime`,
+        `RangeAttackTime`,
+        `unit_class`,
+        `unit_flags`,
+        `type`,
+        `type_flags`,
+        `lootid`,
+        `pickpocketloot`,
+        `skinloot`,
+        `AIName`,
+        `MovementType`,
+        `HoverHeight`,
+        `RacialLeader`,
+        `movementId`,
+        `RegenHealth`,
+        `CreatureImmunitiesId`,
+        `flags_extra`,
+        `ScriptName`
+    ) VALUES
 (@ENTRY, @NAME, "Learn any profession", NULL, 0, 80, 80, 2, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'npc_all_professions_trainer');
